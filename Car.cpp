82ffc45130864abdcdc2e99e2ac05f399913fbca
SirChili <thomastony4628@gmail.com>
2025-09-29 21:02:22 -0500

    Create Car.cpp

---
+++ Car.cpp
@@ -0,0 +1,35 @@
+#include "Car.h"
+
+//Car Variables and Functions
+//________________________________________
+void Car::beginDrive()
+{
+}
+
+//Engine Variables and Functions
+//________________________________________
+void Engine::startEngine()
+{
+}
+
+void Engine::accelerate()
+{
+}
+
+//Tire Variables and Functions
+//________________________________________
+void Tire::inflateTire(int i)
+{
+}
+
+//Door Variables and Functions
+//________________________________________
+void Door::openDoor()
+{
+}
+
+//Window Variables and Functions
+//________________________________________
+void Window::rollDown()
+{
+}
