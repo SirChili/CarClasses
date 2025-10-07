dcce816f16f87301dc915ce25a93e9ce4a0d753e
SirChili <thomastony4628@gmail.com>
2025-10-07 13:49:02 -0500

    Update Car.cpp

--- Car.cpp
+++ Car.cpp
@@ -1,35 +1,69 @@
 #include "Car.h"
 
-//Car Variables and Functions
-//________________________________________
-void Car::beginDrive()
-{
-}
 
 //Engine Variables and Functions
 //________________________________________
-void Engine::startEngine()
+// double engineVolume;
+void  Engine::startEngine()
 {
+	cout << "Starting engine...\n";
 }
 
 void Engine::accelerate()
 {
+	cout << "Accelerating... Vroom Vroom!!\n";
+}
+
+void Engine::setEngineVolume(double e)
+{
+	engineVolume = e;
+}
+
+double Engine::getEngineVolume() 
+{
+	return engineVolume;
 }
 
 //Tire Variables and Functions
 //________________________________________
+//string tireSize;
 void Tire::inflateTire(int i)
-{
-}
-
-//Door Variables and Functions
-//________________________________________
-void Door::openDoor()
-{
+{	
+	cout << "Inflating tire to " << i << " psi...\n";
 }
 
 //Window Variables and Functions
 //________________________________________
 void Window::rollDown()
 {
+	cout << "Rolling down window...\n";
+}
+
+//Door Variables and Functions
+//________________________________________
+//Window windowObject;
+void Door::openDoor()
+{
+	cout << "Opening door...\n";
+}
+
+//Car Variables and Functions
+//________________________________________
+Engine engineObject;
+Tire tireObjects[4];
+Door driverDoor;
+Door passengerDoor;
+
+void Car::beginDrive()
+{
+}
+
+Car::Car()
+{
+}
+
+Car::Car(double ev, string ts)
+{
+	engineObject.engineVolume = ev;
+	tireObjects->tireSize = ts;
 }
