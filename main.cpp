866797bbb87c60f2c44ac78ab041cc183db71d48
SirChili <thomastony4628@gmail.com>
2025-10-07 13:48:34 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include "Car.h"
 
 using namespace std;
 
 int main() {
+	
+	//Engine Volume, Tire Size
+	Car myCar(3.1, "P215/65R15");
 
+	cout << "Engine volume (in liters) is: " << myCar.engineObject.getEngineVolume() << "\n";
+	cout << "All 4 tire specifications are: " << myCar.tireObjects->tireSize << "\n\n\n";
 
-
+	myCar.driverDoor.openDoor();
+	myCar.engineObject.startEngine();
+	myCar.driverDoor.windowObject.rollDown();
+	myCar.tireObjects->inflateTire(32);
+	myCar.engineObject.accelerate();
 }
